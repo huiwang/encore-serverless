49b71c7a5496b218b34cdfa0fca13ca850e93163
Hui Wang <hedonplay@gmail.com>
2018-04-02 23:09:53 +0200

    add validator on duplicates

--- src/main/java/com/truelaurel/recommend/Validator.java
+++ src/main/java/com/truelaurel/recommend/Validator.java
@@ -1,5 +1,8 @@
 package com.truelaurel.recommend;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Validator {
     static void validateRequest(Request request) {
         if (request.getInternal() < 1) {
@@ -7,6 +10,19 @@ public class Validator {
         }
         if (request.getExternal() < 0) {
             throw new IllegalArgumentException("external link must be positive. external=" + request.getExternal());
+        }
+        detectDuplicates(request.getSite());
+    }
+
+    static void detectDuplicates(Site site) {
+        Map<String, Post> posts = new HashMap<>();
+        for (Post post : site.getPosts()) {
+            if (!posts.containsKey(post.getPermalink())) {
+                posts.put(post.getPermalink(), post);
+            } else {
+                throw new IllegalArgumentException(
+                        "Duplicated posts detected between " + posts.get(post.getPermalink()) + " and " + post);
+            }
         }
     }
 }
---
+++ src/test/java/com/truelaurel/recommend/ValidatorTest.java
@@ -0,0 +1,16 @@
+package com.truelaurel.recommend;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class ValidatorTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void detectDuplicates() {
+        Post post11 = new Post("hello world", "http://hui-wang.info/duplicate.html", Arrays.asList("hello", "world"));
+        Post post12 = new Post("hello france", "http://hui-wang.info/duplicate.html", Arrays.asList("hello", "france"));
+        Site site1 = new Site("hui-wang.info", Arrays.asList(post11, post12));
+        Validator.detectDuplicates(site1);
+    }
+}
