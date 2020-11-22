c5e6bed4a9a62daa9e763661e8b31b83d07cf6b1
Hui Wang <hui.wang@contentsquare.com>
2020-11-22 21:34:10 +0100

    clean outdated blog by enabling 30 days ttl

--- src/main/java/com/truelaurel/recommend/Site.java
+++ src/main/java/com/truelaurel/recommend/Site.java
@@ -3,6 +3,7 @@ package com.truelaurel.recommend;
 import software.amazon.awssdk.services.dynamodb.datamodeling.DynamoDbHashKey;
 import software.amazon.awssdk.services.dynamodb.datamodeling.DynamoDbTable;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @DynamoDbTable(tableName = "encore-prod-site")
@@ -11,6 +12,7 @@ public class Site {
     @DynamoDbHashKey
     private String domain;
     private List<Post> posts;
+    private long ttl = ZonedDateTime.now().plusDays(30).toEpochSecond();
 
     public Site() {
     }
@@ -36,12 +38,20 @@ public class Site {
         this.posts = posts;
     }
 
+    public long getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(long ttl) {
+        this.ttl = ttl;
+    }
 
     @Override
     public String toString() {
         return "Site{" +
                 "domain='" + domain + '\'' +
                 ", posts=" + posts +
+                ", ttl=" + ttl +
                 '}';
     }
 }
